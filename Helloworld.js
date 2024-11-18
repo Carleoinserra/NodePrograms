b98efa3fb352f6c9671e0a46c4563a8a4b06ed38
Carleoinserra <inserracarlo@gmail.com>
2024-11-18 17:46:38 +0100

    Create Helloworld.js

---
+++ Helloworld.js
@@ -0,0 +1,21 @@
+/*
+Il primo programma sarà anche il più semplice:
+vogliamo creare un programma che stampi da terminale il messaggio "ciao mondo"
+Per creare un programma con node dobbiamo per prima creare un file con estensione .js
+Una volta che abbiamo installato correttamente node.js sulla nostra macchina dobbiamo verificare
+1. per verificare se node è stato installato correttamente apriamo il terminale in visual 
+(vi consiglio se avete windows di aprire il prompt dei comandi e non prompt di windows)
+diamo il comando node -h
+se code è installato correttamente nel nostro ambiente otteremo un risultato del genere
+Di solito siamo abituati a pensare che l'interprete del nostro script sia il browser
+in questo caso il nostro interprete è il terminale
+Dopo aver creato il file con la giusta estensione
+diamo un semplice comando di console.log("ciao mondo")
+Ora per eseguire il programma dobbiamo aprire il terminale nella cartella dove si trova il file che stiamo scrivendo
+diamo il comando node nomeDelprogramma
+come vedete il nostro output sarà il terminale
+
+*/
+
+
+console.log("Ciao mondo")
