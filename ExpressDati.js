c318421eed7f63c90d9d3a8866b8c1bdac4a5f25
Carleoinserra <inserracarlo@gmail.com>
2024-12-28 11:18:50 +0100

    Add files via upload

--- ExpressDati.js
+++ ExpressDati.js
@@ -12,7 +12,16 @@ come valore una stringa
 dopo aver creato la rotta devo mettere l'applicazione in ascolto su una porta specifica
 lo faccio chiamando sull'oggetto app il metodo listen
 Nel pug dopo aver creato uno schema html semplice tramite indentazione
-vado a creare un elmento h1, figlio di body 
+vado a creare un elmento h1, figlio di body che utilizza la variabile che abbiamo passato da express
+Il risultato è la stampa del valore precedentemente assegnato
+Ora provo a passare come seconda variabile un dato di tipo numerico di nome anni
+vado a stampare la variabile nello stesso h1 di prima
+Nell'ultimo esempio andrò a passare a pug un array di stringhe
+In quasto caso il nome dell'array e della variabile pug coincidono
+Voglio stampare l'array come una lista quindi utilizzerò un elemento ul
+la sintassi sarà each colore: lista li= colore
+
+
 
 
 
